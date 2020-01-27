4ee6ae3f6110ff58eb716cd5c26109ea19dd4f15
Michiel29 <michiel.s.dejong@gmail.com>
2020-01-27 15:14:35 -0800

    reorganize datasets

--- datasets/triplet_dataset.py
+++ datasets/triplet_dataset.py
@@ -1,9 +1,31 @@
 from datasets import RelInfDataset
 
 class TripletDataset(RelInfDataset):
-    def retrieve_subgraph(self, ent1, ent2):
-        return (self.ent_dict[ent1], self.)
-        raise NotImplementedError
 
-    def sample_entities(self, sample, k_negative):
-        raise NotImplementedError
+    def __init__(self, k_negative):
+        self.k_negative = k_negative
+
+    def collater(self, instances):
+    
+        batch = {
+            'mention': [],
+            'head': [],
+            'tail': [],
+            'target': []
+        }
+
+        for instance in instances:
+
+            """Perform Masking"""
+            mention_samples, ent_samples, targets = self.sample_entities(instance, self.k_negative)
+
+            """Retrieve Subgraph"""
+            for head, tail in ent_samples:
+                batch['head'].append(head)
+                batch['tail'].append(tail)
+                
+            batch['mentions'].append(mention_samples)
+            batch['targets'].append(targets)
+
+        return batch
+            
