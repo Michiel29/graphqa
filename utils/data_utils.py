86a2ae7729a3a6275a8ce06e33dd692c0c8c5a31
Michiel29 <michiel.s.dejong@gmail.com>
2020-01-30 11:49:11 -0800

    added dictionary class with entity tokens

---
+++ utils/data_utils.py
@@ -0,0 +1,26 @@
+from fairseq.data import Dictionary
+
+
+class CustomDictionary(Dictionary):
+
+    def __init__(self, head_token='<head>', tail_token = '<tail>', unk_ent_token='<unk_ent>'):
+        super().__init__()
+        self.head_token = head_token
+        self.tail_token = tail_token
+        self.unk_ent_token = unk_ent_token
+
+        self.head_index = self.add_symbol(self.head_token)
+        self.tail_index = self.add_symbol(self.tail_token)
+        self.unk_ent_index = self.add_symbol(unk_ent_token)
+
+    def head(self):
+        return self.head_index
+
+    def tail(self):
+        return self.tail_index        
+
+    def unk_ent(self):
+        return self.unk_ent_index
+
+
+
