b7a5288928cd05067f2fb74565c3ec8bdf403f6b
Michiel29 <michiel.s.dejong@gmail.com>
2020-02-17 13:59:35 -0800

    small refactor of diagnostic

--- utils/diagnostic_utils.py
+++ utils/diagnostic_utils.py
@@ -25,10 +25,12 @@ class Diagnostic():
     filter_tokens = [cd.pad_df]
     replace_tokens = [cd.head_token_df, cd.tail_token_df, cd.unk_ent_token_df]
 
-    def __init__(self):
+    def __init__(self, task):
         self.bpe = get_encoder(self.encoder_json, self.vocab_bpe)
+        self.task = task
 
-    def decode_sentence(self, token_ids):
+    def decode_sentence(self, sentence):
+        token_ids = self.task.dictionary.string(sentence).split()
         processed_ids = []
         for token in token_ids:
             if token in self.replace_tokens:
@@ -40,51 +42,52 @@ class Diagnostic():
 
         return decoded_sentence
 
-    def inspect_batch(self, batch, task, scores=None):
+    def inspect_batch(self, batch, scores=None):
 
         batch_size = batch['batch_size']
         mention_id = batch['mention']
         target = batch['target']
 
-        if task.args.task == 'triplet_inference':
+        if self.task.args.task == 'triplet_inference':
             head_id = batch['head']
             tail_id = batch['tail']
-        elif task.args.task == 'fewrel':
-            n_way = task.args.n_way
-            n_shot = task.args.n_shot
+        elif self.task.args.task == 'fewrel':
+            n_way = self.task.args.n_way
+            n_shot = self.task.args.n_shot
             exemplars_id = batch['exemplars'].reshape(batch_size, n_way, n_shot, -1)
 
         for i in range(batch_size):
 
-            cur_mention_id = task.dictionary.string(mention_id[i]).split()
-            decoded_mention = self.decode_sentence(cur_mention_id)
+            # cur_mention_id = task.dictionary.string(mention_id[i]).split()
+            # decoded_mention = self.decode_sentence(cur_mention_id)
 
-            if task.args.task == 'triplet_inference':
-                pos_head_ent = task.entity_dictionary[head_id[i,0]]
-                pos_tail_ent = task.entity_dictionary[tail_id[i,0]]
-                neg_head_ent = [task.entity_dictionary[head_id[i,j]] for j in range(1, head_id.shape[1])]
-                neg_tail_ent = [task.entity_dictionary[tail_id[i,j]] for j in range(1, tail_id.shape[1])]
+            decoded_mention = self.decode_sentence(mention_id[i])
 
-            elif task.args.task == 'fewrel':
+            if self.task.args.task == 'triplet_inference':
+                pos_head_ent = self.task.entity_dictionary[head_id[i,0]]
+                pos_tail_ent = self.task.entity_dictionary[tail_id[i,0]]
+                neg_head_ent = [self.task.entity_dictionary[head_id[i,j]] for j in range(1, head_id.shape[1])]
+                neg_tail_ent = [self.task.entity_dictionary[tail_id[i,j]] for j in range(1, tail_id.shape[1])]
+
+            elif self.task.args.task == 'fewrel':
                 decoded_exemplars = {}
                 for j in range(n_way):
-                    decoded_exemplars[j] = {}
+                    decoded_exemplars[j] = set()
                     for k in range(n_shot):
-                        cur_exemplar_id = task.dictionary.string(exemplars_id[i,j,k,:]).split()
-                        decoded_exemplars[j][k] = self.decode_sentence(cur_exemplar_id)
+                        # cur_exemplar_id = self.task.dictionary.string(exemplars_id[i,j,k,:]).split()
+                        decoded_exemplars[j].add(self.decode_sentence(exemplars_id[i,j,k,:]))
 
-
-            print('\n\nMENTION ID LIST:\n {}\n'.format(cur_mention_id))
+            print('\n\nMENTION ID LIST:\n {}\n'.format(self.task.dictionary.string(mention_id[i]).split()))
             print('DECODED MENTION:\n {}\n'.format(decoded_mention))
-            if task.args.task == 'triplet_inference':
+            if self.task.args.task == 'triplet_inference':
                 print('POSITIVE <head> ENTITY:\n {} (ID={})\n'.format(pos_head_ent, head_id[i,0].item()))
                 print('POSITIVE <tail> ENTITY:\n {} (ID={})\n'.format(pos_tail_ent, tail_id[i,0].item()))
                 print('NEGATIVE <head> ENTITIES:\n {} (ID={})\n'.format(neg_head_ent, head_id[i,1:].cpu().numpy()))
                 print('NEGATIVE <tail> ENTITIES:\n {} (ID={})\n'.format(neg_tail_ent, tail_id[i,1:].cpu().numpy()))
-            elif task.args.task == 'fewrel':
+            elif self.task.args.task == 'fewrel':
                 print('DECODED EXEMPLARS (w/o ENTITIES):')
                 for j in range(n_way):
-                    print('{}\n'.format(decoded_exemplars[j]))
+                    print('Class {0}: {1}\n'.format(j, decoded_exemplars[j]))
             print('TARGET: \n {}\n'.format(target[i].cpu().detach().numpy()))
             if scores is not None:
                 print('SCORES: \n {}\n'.format(F.softmax(scores[i,:], dim=-1).cpu().detach().numpy()))
