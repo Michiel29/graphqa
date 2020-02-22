13f3a925592c865eeeea7629558f196289e57718
Michiel29 <michiel.s.dejong@gmail.com>
2020-02-21 16:28:52 -0800

    move graph to preprocessing

--- tasks/triplet_inference.py
+++ tasks/triplet_inference.py
@@ -19,19 +19,7 @@ class TripletInferenceTask(RelationInferenceTask):
             split (str): name of the split (e.g., train, valid, test)
         """
 
-        # Don't reload datasets that were already setup earlier
-        if split in self.datasets:
-            return
-
         text_data, annotation_data = self.load_annotated_text(split)
-
-        if split=='train':
-            # here temporarily
-            from utils.data_utils import Graph
-            logger.info('beginning graph construction')
-            self.graph = Graph()
-            self.graph.construct_graph(annotation_data, len(self.entity_dictionary))
-            logger.info('finished graph construction')
 
         self.datasets[split] = TripletDataset(
             text_data,
