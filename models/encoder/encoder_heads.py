7e14ab9ecc6f4e2bf082ee20a2172b9cba618448
Michiel29 <michiel.s.dejong@gmail.com>
2020-02-26 10:41:02 -0800

    moved linear to head

--- models/encoder/encoder_heads.py
+++ models/encoder/encoder_heads.py
@@ -18,6 +18,27 @@ class BoW(nn.Module):
 
         return avg_emb
 
+class BoWLinear(nn.Module):
+
+    def __init__(self, args, dictionary):
+        super().__init__()
+        self.padding_idx = dictionary.pad()
+        self.head_idx = dictionary.head()
+        self.tail_idx = dictionary.tail()
+
+        self.linear = nn.Linear(args.encoder_embed_dim, args.encoder_representation_dim)
+
+    def forward(self, x, src_tokens, **unused):
+        # x: [batch_size, length, enc_dim]
+        mask = ((src_tokens != self.padding_idx) & (src_tokens != self.head_idx) & (src_tokens != self.tail_idx)).unsqueeze(-1) # [batch_size, length, enc_dim]
+        masked_emb = x * mask
+        mask_sum = mask.sum(dim=-2) # [batch_size, enc_dim]
+        avg_emb = torch.sum(masked_emb, dim=-2) / mask_sum
+
+        linear_projection = self.linear(avg_emb)
+
+        return linear_projection
+
 class HeadTailConcat(nn.Module):
 
     def __init__(self, args, dictionary):
@@ -38,7 +59,35 @@ class HeadTailConcat(nn.Module):
 
         return head_tail_concat
 
+
+class HeadTailConcatLinear(nn.Module):
+
+    def __init__(self, args, dictionary):
+        super().__init__()
+        self.head_idx = dictionary.head()
+        self.tail_idx = dictionary.tail()
+
+        self.linear = nn.Linear(2*args.encoder_embed_dim, args.encoder_representation_dim)
+
+    def forward(self, x, src_tokens, **unused):
+        # x: [batch_size, length, enc_dim]
+
+        head_mask = (src_tokens == self.head_idx) # [batch_size, length]
+        tail_mask = (src_tokens == self.tail_idx) # [batch_size, length]
+
+        head_sum = torch.bmm(head_mask.unsqueeze(-2).float(), x).squeeze(-2) / torch.sum(head_mask, dim=-1, keepdim=True) # [batch_size, enc_dim]
+        tail_sum = torch.bmm(tail_mask.unsqueeze(-2).float(), x).squeeze(-2) / torch.sum(tail_mask, dim=-1, keepdim=True) # [batch_size, enc_dim]
+
+        head_tail_concat = torch.cat((head_sum, tail_sum), dim=-1) # [batch_size, 2 * enc_dim]
+
+        linear_projection = self.linear(head_tail_concat)
+
+        return linear_projection
+
+
 encoder_head_dict = {
     'bag_of_words': BoW,
-    'head_tail_concat': HeadTailConcat
+    'bag_of_words_linear': BoWLinear,
+    'head_tail_concat': HeadTailConcat,
+    'head_tail_concat_linear': HeadTailConcatLinear
 }
