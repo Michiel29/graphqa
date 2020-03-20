5a55dc0bf47a7440372ff525672bfb984f394390
Yury Zemlyanskiy <yuri.zemlyanskiy@gmail.com>
2020-03-19 19:32:02 -0700

    add expand mentions into prepare_graph

--- scripts/prepare_graph.py
+++ scripts/prepare_graph.py
@@ -12,8 +12,15 @@ from fairseq.data.data_utils import load_indexed_dataset
 
 def main(args):
     text_data, annotation_data = load_text_annotations(args.data_path, args.prefix)
+
+    sentence_end_pos = text_data.sizes.cumsum()
+    sentence_start_pos = np.roll(sentence_end_pos, shift=1)
+    sentence_start_pos[0] = 0
+    setattr(text_data, 'sentence_start_pos', sentence_start_pos)
+    setattr(text_data, 'sentence_end_pos', sentence_end_pos)
+
     n_entities = len(Dictionary.load(os.path.join(args.data_path, 'entity.dict.txt')))
-    print('{} entities'.format(n_entities))
+    print('-- {} entities'.format(n_entities))
 
     edges = create_graph(
         text_data,
@@ -21,6 +28,7 @@ def main(args):
         n_entities,
         args.document_sep_len,
         args.max_entity_pair_distance,
+        args.max_positions,
     )
 
     graph_path = os.path.join(args.data_path, args.prefix + '.graph')
@@ -59,6 +67,7 @@ def create_graph(
     n_entities,
     document_sep_len,
     max_entity_pair_distance,
+    max_positions,
 ):
     edges = [list() for entity in range(n_entities)]
     # mentions ordered by starting position
@@ -68,8 +77,8 @@ def create_graph(
     assert len(text_data) == len(annotation_data)
     with trange(len(annotation_data), desc='Collecting entity pairs') as progress_bar:
         for sentence_idx in progress_bar:
-            assert len(text_data[sentence_idx]) >= document_sep_len
-            if len(text_data[sentence_idx]) > document_sep_len:
+            assert text_data.sizes[sentence_idx] >= document_sep_len
+            if text_data.sizes[sentence_idx] > document_sep_len:
                 for annotation_index in range(0, len(annotation_data[sentence_idx]), 3):
                     # annotation = (starting position in the sentence, ending position, entity ID)
                     start_pos = annotation_data[sentence_idx][annotation_index].item() + global_text_index
@@ -83,14 +92,25 @@ def create_graph(
                     for current_entity in current_entities:
                         assert abs(start_pos - current_entity[0]) <= max_entity_pair_distance
                         if current_entity[2] != entity:
+                            start_block, end_block = expand_mention(
+                                text_data=text_data,
+                                left_sentence_idx=current_entity[3],
+                                left_start_pos=current_entity[0],
+                                left_end_pos=current_entity[1],
+                                right_sentence_idx=sentence_idx,
+                                right_start_pos=start_pos,
+                                right_end_pos=end_pos,
+                                document_sep_len=document_sep_len,
+                                max_positions=max_positions,
+                            )
                             edges[current_entity[2]].append(
-                                (entity, current_entity[0], current_entity[1], start_pos, end_pos)
+                                (entity, current_entity[0], current_entity[1], start_pos, end_pos, start_block, end_block)
                             )
                             edges[entity].append(
-                                (current_entity[2], start_pos, end_pos, current_entity[0], current_entity[1])
+                                (current_entity[2], start_pos, end_pos, current_entity[0], current_entity[1], start_block, end_block)
                             )
                             num_undirected_edges += 1
-                    current_entities.append((start_pos, end_pos, entity))
+                    current_entities.append((start_pos, end_pos, entity, sentence_idx))
             else:
                 # empty sentence means we hit the end of the document
                 current_entities.clear()
@@ -112,11 +132,74 @@ def create_graph(
     return edges
 
 
+def expand_mention(
+    text_data,
+    left_sentence_idx,
+    left_start_pos,
+    left_end_pos,
+    right_sentence_idx,
+    right_start_pos,
+    right_end_pos,
+    document_sep_len,
+    max_positions,
+):
+    start_pos, end_pos = left_start_pos, right_end_pos
+    if text_data.sentence_end_pos[right_sentence_idx] - text_data.sentence_start_pos[left_sentence_idx] <= max_positions:
+        start_pos = text_data.sentence_start_pos[left_sentence_idx]
+        end_pos = text_data.sentence_end_pos[right_sentence_idx]
+    else:
+        min_symm_expand = min(
+            text_data.sentence_end_pos[right_sentence_idx] - end_pos,
+            start_pos - text_data.sentence_start_pos[left_sentence_idx],
+            int((max_positions - (end_pos - start_pos)) / 2),
+        )
+        start_pos -= min_symm_expand
+        end_pos += min_symm_expand
+        budget_left = max_positions - (end_pos - start_pos)
+        if budget_left > 1:
+            if start_pos == text_data.sentence_start_pos[left_sentence_idx]:
+                end_pos += budget_left
+            elif text_data.sentence_end_pos[right_sentence_idx] == end_pos:
+                start_pos -= budget_left
+            else:
+                raise Exception('Something is bad')
+        assert end_pos - start_pos <= max_positions
+        assert start_pos >= text_data.sentence_start_pos[left_sentence_idx]
+        assert end_pos <= text_data.sentence_end_pos[right_sentence_idx]
+        return start_pos, end_pos
+
+    while end_pos - start_pos < max_positions:
+        if (
+            left_start_pos - start_pos <= end_pos - right_end_pos
+            and (left_sentence_idx > 0)
+            and (end_pos - start_pos + text_data.sizes[left_sentence_idx - 1] <= max_positions)
+            and (text_data.sizes[left_sentence_idx - 1] > document_sep_len)
+        ):
+            left_sentence_idx -= 1
+            start_pos = text_data.sentence_start_pos[left_sentence_idx]
+        elif (
+            (right_sentence_idx + 1 < len(text_data))
+            and (end_pos - start_pos + text_data.sizes[right_sentence_idx + 1] <= max_positions)
+            and (text_data.sizes[right_sentence_idx + 1] > document_sep_len)
+        ):
+            right_sentence_idx += 1
+            end_pos = text_data.sentence_end_pos[right_sentence_idx]
+        else:
+            break
+
+    # May be want to expand to not full sentences later
+    assert end_pos - start_pos <= max_positions
+    assert start_pos >= text_data.sentence_start_pos[left_sentence_idx]
+    assert end_pos <= text_data.sentence_end_pos[right_sentence_idx]
+    return start_pos, end_pos
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Construction of IndexedDatasets for graph neighbors and edges')
     parser.add_argument('--data-path', type=str, help='Data directory', default='../data/bin_sample')
     parser.add_argument('--prefix', type=str)
     parser.add_argument('--document-sep-len', type=int)
+    parser.add_argument('--max-positions', type=int)
     parser.add_argument('--max-entity-pair-distance', type=int)
 
     args = parser.parse_args()
