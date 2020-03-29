b54eeb390a719f93a8b7846fd5031748f2370d79
aarzchan <aaronzchan@gmail.com>
2020-03-28 23:01:33 -0700

    updated semeval

--- tasks/semeval2010task8.py
+++ tasks/semeval2010task8.py
@@ -13,16 +13,17 @@ from fairseq.tasks import register_task
 
 from tasks import BaseTask
 from datasets import (
-    AnnotatedTextDataset,
+    AnnotatedText,
     SemEval2010Task8Dataset,
     FilteredDataset,
+    PrependTokenDataset,
     prune_dataset_size,
 )
 from utils.data_utils import (
-    CustomDictionary,
     load_annotated_text,
     safe_load_indexed_dataset,
 )
+from utils.dictionary import CustomDictionary
 
 logger = logging.getLogger(__name__)
 
@@ -46,49 +47,38 @@ class SemEval2010Task8Task(BaseTask):
         return cls(args, dictionary, None)
 
     def load_dataset(self, split, epoch=0, combine=False, **kwargs):
-        text_data, annotation_data = load_annotated_text(
-            self.args.data_path,
-            split,
-            self.dictionary.bos(),
+        text_data = safe_load_indexed_dataset(
+            os.path.join(self.args.data_path, split + '.text'),
         )
-        relation_data = safe_load_indexed_dataset(
-            os.path.join(self.args.data_path, split + '.relations')
+        annotation_data = np.load(
+            os.path.join(self.args.data_path, split + '.annotations.npy'),
+            mmap_mode='r',
         )
-        annotated_text_dataset = AnnotatedTextDataset(
+        annotated_text = AnnotatedText(
             text_data=text_data,
             annotation_data=annotation_data,
             dictionary=self.dictionary,
-            entity_dictionary=self.entity_dictionary,
-            shift_annotations=1,
             mask_type=self.args.mask_type,
-            assign_head_tail='first',
-            seed=self.seed,
-            alpha=self.args.alpha,
+            non_mask_rate=self.args.non_mask_rate,
         )
-        annotated_text_dataset, indices = self.filter_by_max_positions(
-            annotated_text_dataset,
-            return_indices=True,
+        relation_dataset = safe_load_indexed_dataset(
+            os.path.join(self.args.data_path, split + '.relations')
         )
-        relation_dataset = FilteredDataset(relation_data, indices)
-
-        n_examples = int(getattr(self.args, 'n_' + split + '_examples'))
-        if n_examples > 0:
-            annotated_text_dataset, indices = prune_dataset_size(
-                annotated_text_dataset,
-                n_examples,
-                self.seed,
-                return_indices=True,
-            )
-            relation_dataset = FilteredDataset(relation_dataset, indices)
 
         dataset = SemEval2010Task8Dataset(
-            annotation_text_dataset=annotated_text_dataset,
+            annotation_text=annotated_text,
             relation_dataset=relation_dataset,
             dictionary=self.dictionary,
-            entity_dictionary=self.entity_dictionary,
-            mask_type=self.mask_type,
             seed=self.seed,
         )
+        dataset = PrependTokenDataset(dataset, self.dictionary.bos(), ['text', 'exemplars'])
+
+        n_examples = getattr(self.args, 'n_' + split + '_examples', None)
+        if n_examples is not None:
+            dataset = prune_dataset_size(
+                dataset,
+                n_examples,
+                self.seed,
+            )
 
         self.datasets[split] = dataset
-        
