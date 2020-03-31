f9ea0b9f4502c5e970b4b00d21205a09edf6cb9c
Yury Zemlyanskiy <yuri.zemlyanskiy@gmail.com>
2020-03-30 18:55:11 -0700

    entity_start_mlp

--- models/encoder/encoder_heads.py
+++ models/encoder/encoder_heads.py
@@ -121,6 +121,44 @@ class EntityStartLayerNorm(nn.Module):
 
         return output
 
+
+class EntityStartMLP(nn.Module):
+
+    def __init__(self, args, dictionary):
+        super().__init__()
+        if args.mask_type == 'head_tail':
+            self.head_idx = dictionary.head()
+            self.tail_idx = dictionary.tail()
+        elif args.mask_type == 'start_end':
+            self.head_idx = dictionary.e1_start()
+            self.tail_idx = dictionary.e2_start()
+        else:
+            raise Exception('EntityStarMLP is unsupported for the mask type %s' % str(args.mask_type))
+
+        self.linear = nn.Linear(2 * args.encoder_embed_dim, args.entity_dim)
+        self.activation_fn = nn.Tanh()
+        # self.dropout = nn.Dropout()
+        self.out_proj = nn.Linear(args.entity_dim, args.entity_dim)
+
+    def forward(self, x, src_tokens, **unused):
+        # x: [batch_size, length, enc_dim]
+
+        head_mask = (src_tokens == self.head_idx) # [batch_size, length]
+        tail_mask = (src_tokens == self.tail_idx) # [batch_size, length]
+
+        head_sum = torch.bmm(head_mask.unsqueeze(-2).float(), x).squeeze(-2) / torch.sum(head_mask, dim=-1, keepdim=True) # [batch_size, enc_dim]
+        tail_sum = torch.bmm(tail_mask.unsqueeze(-2).float(), x).squeeze(-2) / torch.sum(tail_mask, dim=-1, keepdim=True) # [batch_size, enc_dim]
+
+        head_tail_concat = torch.cat((head_sum, tail_sum), dim=-1) # [batch_size, 2 * enc_dim]
+
+        # x = self.dropout(head_tail_concat)
+        x = self.linear(head_tail_concat)
+        x = self.activation_fn(x)
+        # x = self.dropout(x)
+        x = self.out_proj(x)
+        return x
+
+
 class EntityPoolingFirstToken(nn.Module):
 
     def __init__(self, args, dictionary):
@@ -165,6 +203,7 @@ encoder_head_dict = {
     'bag_of_words_linear': BoWLinear,
     'entity_start': EntityStart,
     'entity_start_linear': EntityStartLinear,
+    'entity_start_mlp': EntityStartMLP,
     'entity_start_layer_norm': EntityStartLayerNorm,
     'entity_pooling_first_token': EntityPoolingFirstToken,
     'cls_token_linear': CLSTokenLinear,
