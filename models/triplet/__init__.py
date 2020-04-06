029d78a6b948ed5c4babc14cbcf737006dd437f0
Yury Zemlyanskiy <yuri.zemlyanskiy@gmail.com>
2020-04-05 17:30:42 -0700

    ConcatLinearDot

--- models/triplet/__init__.py
+++ models/triplet/__init__.py
@@ -1,7 +1,8 @@
-from .dist_mult import ConcatDot, DistMult, DistMultEntityOnly, RotatE
+from .dist_mult import ConcatDot, ConcatLinearDot, DistMult, DistMultEntityOnly, RotatE
 
 triplet_dict = {
     'concat_dot': ConcatDot,
+    'concat_linear_dot': ConcatLinearDot,
     'distmult': DistMult,
     'distmult_entity_only': DistMultEntityOnly,
     'rotate': RotatE,
--- models/triplet/dist_mult.py
+++ models/triplet/dist_mult.py
@@ -34,3 +34,14 @@ class ConcatDot(nn.Module):
     def forward(self, mention, head, tail):
         head_tail_emb = torch.cat([head, tail], axis=-1)
         return torch.bmm(head_tail_emb, mention.unsqueeze(-1)).squeeze(-1)
+
+
+class ConcatLinearDot(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.linear = nn.Linear(2 * args.entity_dim, args.entity_dim)
+
+    def forward(self, mention, head, tail):
+        head_tail_emb = torch.cat([head, tail], axis=-1)
+        head_tail_emb = self.linear(head_tail_emb)
+        return torch.bmm(head_tail_emb, mention.unsqueeze(-1)).squeeze(-1)
