f85e4fd1f9b2ab0f07135db00756a0b07b3cebd1
Yury Zemlyanskiy <yuri.zemlyanskiy@gmail.com>
2020-04-05 17:54:03 -0700

    remove annoying logging from utils/numpy_utils.py

--- utils/numpy_utils.py
+++ utils/numpy_utils.py
@@ -1,18 +1,10 @@
-import logging
 import numpy as np
-
-
-logger = logging.getLogger(__name__)
 
 
 class MMapNumpyArray(object):
     def __init__(self, path):
         self.path = path
         self.array = np.load(self.path, mmap_mode='r')
-        logger.info('memory mapped array of shape %s from %s' % (
-            str(self.array.shape),
-            self.path,
-        ))
 
     def __getstate__(self):
         return self.path
