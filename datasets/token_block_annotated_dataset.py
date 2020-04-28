df720556c494d1bbee86f24ef708f11030c17199
Yury Zemlyanskiy <yuri.zemlyanskiy@gmail.com>
2020-04-27 22:54:43 -0700

    custom TokenBlockAnnotatedDataset

--- datasets/token_block_annotated_dataset.py
+++ datasets/token_block_annotated_dataset.py
@@ -1,16 +1,18 @@
 import logging
 import numpy as np
 
-from fairseq.data import TokenBlockDataset
+from fairseq.data import FairseqDataset
+from fairseq.data.token_block_utils_fast import _get_slice_indices_fast
 
 from datasets import AnnotatedText
 from utils.data_utils import numpy_seed
+from utils.plasma_utils import maybe_move_to_plasma
 
 
 logger = logging.getLogger(__name__)
 
 
-class TokenBlockAnnotatedDataset(TokenBlockDataset):
+class TokenBlockAnnotatedDataset(FairseqDataset):
     def __init__(
         self,
         annotated_text,
@@ -20,17 +22,26 @@ class TokenBlockAnnotatedDataset(TokenBlockDataset):
         document_sep_len,
         seed,
     ):
-        super().__init__(
-            dataset=annotated_text,
-            sizes=annotated_text.sizes,
-            block_size=max_positions,
-            pad=pad,
-            eos=eos,
+        super().__init__()
+        self.dataset = annotated_text
+        self.pad = pad
+        self.eos = eos
+        self.max_positions = max_positions
+
+        assert len(self.dataset) > 0
+        sizes = annotated_text.sizes.astype(np.int64)
+
+        slice_indices = _get_slice_indices_fast(
+            sizes=sizes,
             break_mode='complete_doc',
-            include_targets=False,
+            block_size=self.max_positions,
             document_sep_len=document_sep_len,
         )
-        self.max_positions = max_positions
+        _sizes = slice_indices[:, 1] - slice_indices[:, 0]
+
+        self._slice_indices = maybe_move_to_plasma(slice_indices)
+        self._sizes = maybe_move_to_plasma(_sizes)
+
         self.seed = seed
         self.epoch = 0
 
@@ -55,12 +66,8 @@ class TokenBlockAnnotatedDataset(TokenBlockDataset):
 
     def __getitem__(self, index):
         with numpy_seed('TokenBlockAnnotatedDataset', self.seed, self.epoch, index):
-            start_ds_idx, start_offset, end_ds_idx = self.block_to_dataset_index[index]
-            slice_s, slice_e = self.slice_indices[index]
-            length = min(slice_e - slice_s, self.max_positions)
-
-            start_block = self.dataset.sentence_offsets.array[start_ds_idx] + start_offset
-            end_block = start_block + length
+            start_block, end_block = self._slice_indices.array[index]
+            end_block = min(start_block + self.max_positions, end_block)
 
             annotations = self.dataset.annotations_block(start_block, end_block)
             if len(annotations) > 0:
@@ -96,3 +103,10 @@ class TokenBlockAnnotatedDataset(TokenBlockDataset):
     def size(self, index):
         # TODO: Note, this is an overestimation of the actual number of tokens
         return min(self._sizes.array[index], self.max_positions + 4)
+
+    def __len__(self):
+        return len(self._slice_indices.array)
+
+    @property
+    def supports_prefetch(self):
+        return False
