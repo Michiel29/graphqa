fix for full softmax

datasets/etp_downstream_dataset.py
@@ -17,7 +17,6 @@ class ETPDownstreamDataset(BaseWrapperDataset):
         edges,
         dictionary,
         n_entities,
-        total_negatives,
         seed,
         split):
 
@@ -25,7 +24,6 @@ class ETPDownstreamDataset(BaseWrapperDataset):
         self.edges = edges
         self.dictionary = dictionary
         self.n_entities = n_entities
-        self.total_negatives = total_negatives
         self.seed = seed
         self.split = split
 
@@ -39,18 +37,7 @@ class ETPDownstreamDataset(BaseWrapperDataset):
 
     def __getitem__(self, index, **kwargs):
         item = self.dataset.__getitem__(index, **kwargs)
-        answer_entity = item['answer']
-        with data_utils.numpy_seed('etp_downstream', self.seed, self.epoch, index):
-            negatives = np.random.choice(self.n_entities, replace=False, size=self.total_negatives)
-            candidates = np.concatenate(([answer_entity], negatives))
-            item['candidates'] = candidates
-
-        if self.split == 'train':
-            target = 0
-        else:
-            target = answer_entity
-
-        item['target'] = target
+        item['target'] = item['answer']
 
         text = item['question']
         mask_token = torch.LongTensor([self.dictionary.blank()])
@@ -79,13 +66,12 @@ class ETPDownstreamDataset(BaseWrapperDataset):
         if batch_size == 0:
             return None
 
-        text, annotation, candidates, target = [], [], [], []
+        text, annotation, target = [], [], []
         ntokens, nsentences = 0, 0
 
         for instance in instances:
             text.append(instance['text'])
             annotation.append(instance['annotation'])
-            candidates.append(instance['candidates'])
             target.append(instance['target'])
             ntokens += len(instance['text'])
             nsentences += 1
@@ -101,7 +87,6 @@ class ETPDownstreamDataset(BaseWrapperDataset):
             'text': padded_text,
             'target': torch.LongTensor(target),
             'annotation': annotation,
-            'candidates': torch.LongTensor(candidates),
             'ntokens': ntokens,
             'nsentences': nsentences,
             'size': batch_size,